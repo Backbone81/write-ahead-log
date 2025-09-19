9c3bb4bcbf2b27986f01ac19bb944d7de1607159
Steffen Rauh <steffen.rauh@gmx.de>
2025-09-19 21:53:58 +0200

    Improved line breaks of example code to prevent scrolling to the right

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -15,42 +15,48 @@ func main() {
 func runDemo() error {
 	walDir := "."
 
-	// Make sure the write-ahead log is initialized. This means that at least one wal file is in the wal directory.
+	// Make sure the write-ahead log is initialized. This means that
+	// at least one wal file is in the wal directory.
 	if err := wal.InitIfRequired(walDir); err != nil {
 		return err
 	}
 
-	// This is the sequence number which you store somewhere else. For this example, we always assume 0 but in reality
-	// this should change whenever you flush your own state to disk.
+	// This is the sequence number which you store somewhere else. For
+	// this example, we always assume 0 but in reality this should
+	// change whenever you flush your own state to disk.
 	var sequenceNumber uint64 = 0
 
-	// Before we can write to the write-ahead log, we need to read all entries starting from our well-known sequence
-	// number.
+	// Before we can write to the write-ahead log, we need to read all
+	// entries starting from our well-known sequence number.
 	log.Println("Opening write-ahead log for reading")
 	reader, err := wal.NewReader(walDir, sequenceNumber)
 	if err != nil {
 		return err
 	}
 	for reader.Next() {
-		// We would use reader.Value().Data and apply it to our saved state. reader.Next will return false when we
-		// have reached the end of the write-ahead log.
+		// We would use reader.Value().Data and apply it to our saved
+		// state. reader.Next will return false when we have reached
+		// the end of the write-ahead log.
 	}
 
-	// We convert the reader to a writer. Note that this is only possible after having read all entries. Afterward the
-	// reader cannot be used anymore.
+	// We convert the reader to a writer. Note that this is only
+	// possible after having read all entries. Afterward the reader
+	// cannot be used anymore.
 	log.Println("Opening write-ahead log for writing")
 	writer, err := reader.ToWriter()
 	if err != nil {
 		return err
 	}
 
-	// We can now append entries. This can also happen concurrently from multiple go routines.
+	// We can now append entries. This can also happen concurrently
+	// from multiple go routines.
 	log.Println("Appending entry")
 	if _, err := writer.AppendEntry([]byte("foo")); err != nil {
 		return err
 	}
 
-	// At the end we should close the writer to make sure that all data was flushed to stable storage.
+	// At the end we should close the writer to make sure that all
+	// data was flushed to stable storage.
 	if err := writer.Close(); err != nil {
 		return err
 	}
